63b7504f37f3a3d065a7a88e2a6debd7eee4cf3d
ramp-member <v279136@gmail.com>
2025-10-05 21:24:17 +0300

    Add files via upload

---
+++ dz4.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int main() {
+    int x, y;
+    setlocale(LC_ALL, "Rus");
+    x = 1;
+    y = 0;
+    if (x != y) {
+        printf("Дверь открывается\n");
+    }
+    else {
+        printf("Ошибка\n");
+    }
+    return 0;
+}
